0dcd879bba3ef067781a37e278ed991bf7790c84
Edvdvlies <edvdvlies@gmail.com>
2020-06-12 11:58:27 +0200

    banner

---
+++ online-leeromgeving/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+    $('a.thumb').click(function(event){
+    	event.preventDefault();
+    	var content = $('.modal-body');
+    	content.empty();
+      	var title = $(this).attr("title");
+      	$('.modal-title').html(title);      	
+      	content.html($(this).html());
+      	$(".modal-profile").modal({show:true});
+    });
+
+  });
